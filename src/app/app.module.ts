05210f8e61192ab1ab88275f8c05d2bb504d4b69
sasha-qred <sashaqred@ya.ru>
2019-07-10 22:08:40 +0300

    feat: add blank routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+
+const routes: Route[] = [];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
